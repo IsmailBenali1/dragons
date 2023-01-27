90bc727be64d7041c4725648352157dcd3a64f9a
ismail B <ismail.benali1@gmail.com>
2023-01-27 13:45:41 +0000

    ajout-d

--- src/actions/actions-types.js
+++ src/actions/actions-types.js
@@ -1,3 +1,4 @@
 // 3ÈME PARTIE
 // gérer les imports
 // récupération des types d'actions
+export const ADD_DRAGON = "ADD_DRAGON";
--- src/constants/actions.js
+++ src/constants/actions.js
@@ -1,2 +1,9 @@
 // 2ÈME PARTIE
 // Créer les constantes des différents type d'actions
+import { ADD_DRAGON } from "./actionTypes";
+
+export function addAction() {
+  return {
+    type: ADD_DRAGON,
+  };
+}
